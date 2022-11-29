10f900620f52d1b8431d15b7f95263a722f2b2c9
jcarlosmariscal <juanc.ramirezm16@gmail.com>
2022-11-28 19:19:46 -0600

    Probando mismo sw

--- sw.js
+++ sw.js
@@ -1,6 +1,7 @@
 importScripts(
   "https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js"
 );
+importScripts("https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js");
 workbox.precaching.precacheAndRoute([
   "index.html",
   "conversion.html",
