e6be1c0ee8345079fa4b949c3a27c1c30709ff0b
jcarlosmariscal <juanc.ramirezm16@gmail.com>
2022-11-28 19:30:57 -0600

    cambios

--- sw.js
+++ sw.js
@@ -1,7 +1,6 @@
 importScripts(
   "https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js"
 );
-importScripts("https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js");
 workbox.precaching.precacheAndRoute([
   "index.html",
   "conversion.html",
